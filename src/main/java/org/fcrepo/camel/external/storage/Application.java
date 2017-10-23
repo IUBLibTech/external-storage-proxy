6402b1dd979e23b09d92bceaa941bdeb46f5e45d
Randall Floyd <rdfloyd@indiana.edu>
2017-10-23 14:04:31 -0400

    Getting REST DSL working in main app and injecting XML DSL routes

--- src/main/java/org/fcrepo/camel/external/storage/Application.java
+++ src/main/java/org/fcrepo/camel/external/storage/Application.java
@@ -1,9 +1,12 @@
 package org.fcrepo.camel.external.storage;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Component;
 
@@ -15,45 +18,25 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 	
+	@Bean
+	    public ServletRegistrationBean camelServletRegistrationBean() {
+	        ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/*");
+	        registration.setName("CamelServlet");
+	        return registration;
+	}
+	
 	@Component
 	    class RestApi extends RouteBuilder {
 
 	        @Override
 	        public void configure() {
 	            restConfiguration()
-	                .contextPath("/").apiContextPath("/api-doc")
-	                    .apiProperty("api.title", "Camel REST API")
-	                    .apiProperty("api.version", "1.0")
-	                    .apiProperty("cors", "true")
-	                    .apiContextRouteId("doc-api")
-	                .bindingMode(RestBindingMode.json);
-
-	   /* TODO Translate this REST XML DSL into Java DSL        
-	    * 
-	    *     <restConfiguration component="jetty" bindingMode="json" port="9091"/>
-      <componentProperty key="foo" value="123"/>
-      <dataFormatProperty key="prettyPrint" value="true"/>
-    </restConfiguration>
-
-    <rest path="/">
-      <get uri="/{service}/status/{external_uri}">
-        <to uri="direct:status_header"/>
-      </get>
-      
-      <post uri="/{service}/stage/{external_uri}">
-        <to uri="direct:stage_header"/>
-      </post>
-    </rest>
-
-	    * 
-	    * rest("/books").description("Books REST service")
-	                .get("/").description("The list of all the books")
-	                    .route().routeId("books-api")
-	                    .bean(Database.class, "findBooks")
-	                    .endRest()
-	                .get("order/{id}").description("Details of an order by id")
-	                    .route().routeId("order-api")
-	                    .bean(Database.class, "findOrder(${header.id})");
-*/	        }
+	            .component("servlet")
+	            .bindingMode(RestBindingMode.json)
+	            .dataFormatProperty("prettyPrint", "true")
+	            .apiContextPath("/api-doc")
+	                .apiProperty("api.title", "User API").apiProperty("api.version", "1.0.0")
+	                .apiProperty("cors", "true");
+	        }
 	}
 }
