ff2eaee586fabb0881e1caed0d8388e78f48afc2
Randall Floyd <rdfloyd@indiana.edu>
2018-01-09 15:47:58 -0500

    Adding tests for processing pending SDA jobs with JobService

--- src/test/java/org/fcrepo/camel/external/storage/provider/sda/ProviderSdaTest.java
+++ src/test/java/org/fcrepo/camel/external/storage/provider/sda/ProviderSdaTest.java
@@ -2,48 +2,33 @@ package org.fcrepo.camel.external.storage.provider.sda;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
-import static com.github.tomakehurst.wiremock.client.WireMock.verify;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.Scanner;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
-// import org.apache.camel.test.junit4.CamelTestSupport;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.assertj.core.api.Assertions;
 import org.fcrepo.camel.external.storage.model.Job;
 import org.fcrepo.camel.external.storage.repository.JobJpaRepository;
 import org.fcrepo.camel.external.storage.service.DatabaseService;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
-import junit.framework.Assert;
 
+//import static org.assertj.core.api.Assertions.assertThat;  // main one
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -57,14 +42,32 @@ import junit.framework.Assert;
 
 public class ProviderSdaTest extends CamelTestSupport {
 
-    private TestRestTemplate restTemplate;
-    
-    
     @Autowired
     JobJpaRepository jobs;
     
+    @Autowired
+    JobService jobService;
+    
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(8101);
+    
+    @Override
+    protected JndiRegistry createRegistry() throws Exception {
+        JndiRegistry registry = super.createRegistry();
+        registry.bind("jobService", jobService);
+        return registry;
+    }
+    
+    Iterable<Job> testJobs = null;
+    
+    @Before
+    public void executedBeforeEach() {
+        DatabaseService dbService = new DatabaseService();
+        Job testJob = dbService.generateJob("testUri", "test", "test");
+        testJob.setStatus("test_started");
+        jobs.save(testJob);
+        testJobs = jobs.findByExternalUriEquals("testUri");
+    }
     
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
@@ -72,108 +75,69 @@ public class ProviderSdaTest extends CamelTestSupport {
 
             @Override
             public void configure() throws Exception {
+                
                 from("direct:startpoint").id("route1") //
                         .process(new Processor() {
-
                             @Override
                             public void process(Exchange exchng) throws Exception {
-                               // Place List containing one job on exchange here
-                                
-                                DatabaseService dbService = new DatabaseService();
-                                Job testJob = dbService.generateJob("testUri", "test", "test");
-                                testJob.setStatus("test_started");
-                                jobs.save(testJob);
-                                Iterable<Job> testJobs = jobs.findByExternalUriEquals("testUri");
-                                log.info("running test with Job: " + testJob.getExternalUri() + ":" + testJob.getStatus());
-                                exchng.getOut().setBody(testJobs);
+                                Iterable<Job> body = exchng.getIn().getBody(Iterable.class);
+                                exchng.getOut().setBody(body);
                             }
                         })
-                        // Call JobService that will ping mocked SDA service and update job 
-                        .process(new JobService());
+                        .to("bean:jobService");
 
                 from("direct:sdaGetCache")
-                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-                 .to("jetty:http://localhost:8101/cacheManager.cgi/cache/testUri");        
+                        .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+                        // See tests below how this call gets mocked with Wiremock stubFor
+                        .to("jetty:http://localhost:8101/cacheManager.cgi/cache/testUri");        
             }
         };
     }
 
-
-    
-    @Ignore
+    //@Ignore
     @Test
-    public void sdaJobPendingTest() throws InterruptedException {
+    public void sdaJobPendingSuccessTest() throws InterruptedException {
+        // This WireMock stub will intercept the jetty HTTP request from within JobService
         stubFor(get(urlPathMatching("/cacheManager.cgi/.*"))
                 .willReturn(aResponse()
                 .withStatus(200)
                 .withHeader("Content-Type", "application/json")
-                .withBody("\"status\": \"test_completed\"")));
-        log.info("running test");
-        // Call endpoint with a List containing one job
-        template.requestBody("direct:startpoint", "<root><name>abc</name></root>");
-        // Status for job in the database should now be test_completed
-        // assertThat(jobs).element(0)
-        //   .hasFieldOrPropertyWithValue("status", "test_completed");
-    }
-    
-    @Test
-    // @Ignore
-    public void wireMockSampleTest() {
-        stubFor(get(urlPathMatching("/cacheManager.cgi/.*"))
-                .willReturn(aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/json")
-                .withBody("\"status\": \"failed\"")));
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet request = new HttpGet("http://localhost:8101/cacheManager.cgi/cache/foo");
-        String stringResponse = "";
-        try {
-            HttpResponse httpResponse = httpClient.execute(request);
-            stringResponse = convertHttpResponseToString(httpResponse);
-            verify(getRequestedFor(urlEqualTo("/cacheManager.cgi/cache/foo")));
-            Assert.assertEquals(200, httpResponse.getStatusLine().getStatusCode());
-            Assert.assertEquals("application/json", httpResponse.getFirstHeader("Content-Type").getValue());
-            Assert.assertEquals("\"status\": \"failed\"", stringResponse);
-        } catch(IOException ie) {
-            
-        }
-    }
-    
-    
-    @Test
-    @Ignore
-    public void sdaJobStatusTest() {
-        ResponseEntity<String> stageResponse = restTemplate.exchange("/sda/stage/foo",
-                                                                    HttpMethod.POST, null, String.class);
+                .withBody("{\"error\": \"0\", \"message\": \"job_completed\", \"url\": \"NA\"}")));
         
-        // Wait for the job_queue_processor route to consume/alter the db rows
-        // assertThat(stageResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        ResponseEntity<List<Job>> response = restTemplate.exchange("/jobs",
-            HttpMethod.GET, null, new ParameterizedTypeReference<List<Job>>() {
-            });
-        List<Job> jobs = response.getBody();
-        /* If the Camel JPA routes are working, the status of selected rows in the database 
-           will eventually be changed from waiting to queued to pending. */
-        // assertThat(jobs).element(0)
-        //   .hasFieldOrPropertyWithValue("status", "pending");
+        
+        // Call the test endpoint with a List containing one job
+        template.requestBody("direct:startpoint", testJobs);
+        // Fields for the job should have changed after being processed by JobService
+        testJobs = jobs.findByExternalUriEquals("testUri");
+        Job myJob = testJobs.iterator().next();
+        Assertions.assertThat(myJob)
+           .hasFieldOrPropertyWithValue("status", "success");
+        Assertions.assertThat(myJob)
+        .hasFieldOrPropertyWithValue("vendorMsg", "job_completed");
+    }
+    
+    //@Ignore
+    @Test
+    public void sdaJobPendingFailedTest() throws InterruptedException {
+        // This WireMock stub will intercept the jetty HTTP request from within JobService
+        stubFor(get(urlPathMatching("/cacheManager.cgi/.*"))
+                .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBody("{\"error\": \"3\", \"message\": \"job_failed\", \"url\": \"NA\"}")));
+        
+        // Call the test endpoint with a List containing one job
+        template.requestBody("direct:startpoint", testJobs);
+        // Fields for the job should have changed after being processed by JobService
+        testJobs = jobs.findByExternalUriEquals("testUri");
+        Job myJob = testJobs.iterator().next();
+        Assertions.assertThat(myJob)
+           .hasFieldOrPropertyWithValue("status", "failed");
+        Assertions.assertThat(myJob)
+        .hasFieldOrPropertyWithValue("vendorMsg", "job_failed");
+    }
 
-    }
     
-    private String convertHttpResponseToString(HttpResponse httpResponse) throws IOException {
-        InputStream inputStream = httpResponse.getEntity().getContent();
-        return convertInputStreamToString(inputStream);
-    }
-    
-    private String convertInputStreamToString(InputStream inputStream) {
-        Scanner scanner = new Scanner(inputStream, "UTF-8");
-        String string = scanner.useDelimiter("\\Z").next();
-        scanner.close();
-        return string;
-    }
+
     
 }
